995bb6982eb52a9c9525119ffd168ce15acec466
ashraf609 <ettqftqfi2222@gmail.com>
2024-09-14 02:37:47 +0200

    new update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -18,65 +18,75 @@ const Work = () => {
 
       <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
         <div className="aspect-square bg-[url('./assets/work-1.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
-          <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-            <div>
-              <h2 className="font-semibold">Projet Frontend</h2>
-              <p className="text-sm text-gray-700">Design Web</p>
+          <a href="https://github.com/samaholicc/systeme-de-livraison">
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+              <div>
+                <h2 className="font-semibold">System de livraison</h2>
+                <p className="text-sm text-gray-700">Technologie : Laravel</p>
+              </div>
+              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                <img src={send_icon} alt="Send Icon" className="w-5" />
+              </div>
             </div>
-            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-              <img src={send_icon} alt="" className="w-5" />
-            </div>
-          </div>
+          </a>
         </div>
 
         <div className="aspect-square bg-[url('./assets/work-2.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
-          <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-            <div>
-              <h2 className="font-semibold">
-                Application basée sur la géolocalisation
-              </h2>
-              <p className="text-sm text-gray-700">Application mobile</p>
+          <a href="https://github.com/samaholicc/star_services_1">
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+              <div>
+                <h2 className="font-semibold">Star Services</h2>
+                <p className="text-sm text-gray-700">Application Web</p>
+              </div>
+              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                <img src={send_icon} alt="Send Icon" className="w-5" />
+              </div>
             </div>
-            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-              <img src={send_icon} alt="" className="w-5" />
-            </div>
-          </div>
+          </a>
         </div>
 
         <div className="aspect-square bg-[url('./assets/work-3.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
-          <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-            <div>
-              <h2 className="font-semibold">Site de photographie</h2>
-              <p className="text-sm text-gray-700">Design Web</p>
+          <a href="https://github.com/samaholicc/seomaster-1.0.0">
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+              <div>
+                <h2 className="font-semibold">Site Vitrine</h2>
+                <p className="text-sm text-gray-700">Design Web</p>
+              </div>
+              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                <img src={send_icon} alt="Send Icon" className="w-5" />
+              </div>
             </div>
-            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-              <img src={send_icon} alt="" className="w-5" />
-            </div>
-          </div>
+          </a>
         </div>
 
         <div className="aspect-square bg-[url('./assets/work-4.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
-          <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-            <div>
-              <h2 className="font-semibold">Conception UI/UX</h2>
-              <p className="text-sm text-gray-700">Design UI/UX</p>
+          <a href="https://github.com/samaholicc/pfa">
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+              <div>
+                <h2 className="font-semibold">Application Mobile</h2>
+                <p className="text-sm text-gray-700">Technologie : Flutter</p>
+              </div>
+              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                <img src={send_icon} alt="Send Icon" className="w-5" />
+              </div>
             </div>
-            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-              <img src={send_icon} alt="" className="w-5" />
-            </div>
-          </div>
+          </a>
         </div>
       </div>
 
       <a
-        href="#"
+        href="https://link-to-more-projects.com"
         className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover"
       >
         Voir plus
-        <img src={right_arrow_blod} alt="" className="w-4 dark:hidden" />
+        <img
+          src={right_arrow_blod}
+          alt="Right Arrow"
+          className="w-4 dark:hidden"
+        />
         <img
           src={right_arrow_blod_dark}
-          alt=""
+          alt="Right Arrow Dark"
           className="w-4 hidden dark:block"
         />
       </a>
