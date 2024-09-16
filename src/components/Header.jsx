fd11ddc8bf56c3d961ee3d56cf5c2102666760ac
samaholicc <Samaholiccs@gmail.com>
2024-09-16 16:20:30 +0200

    modif3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
           />
         </a>
         <a
-          href={Tsynthese} // Lien correct vers le tableau synthese PDF
+          href={tableau_de_synthese.xlsx} // Lien correct vers le tableau synthese PDF
           download="6- Annexe 6-1 - Tableau de syntheÌse - Epreuve E4 - BTS SIO 2023.xlsx" // Définir un nom de fichier correct pour le téléchargement
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
