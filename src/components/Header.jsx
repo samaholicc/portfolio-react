a44132195a12be110f876753f53e794413e53a8b
samaholicc <Samaholiccs@gmail.com>
2024-09-16 15:02:17 +0200

    modif1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <img src={profile_img} alt="Profil" className="rounded-full w-32 mt-40" />
 
       <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
-        Bonjour ! Je m'appelle Samia Boutezrout{" "}
+        Bonjour ! Je m'appelle Samia Boutezrout
         <img src="./assets/hand-icon.png" alt="" className="w-6" />
       </h3>
       <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
